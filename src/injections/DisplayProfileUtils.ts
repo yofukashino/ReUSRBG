cb97ec209fc27a8f243ccbc937e4d136df5728bc
Tharki-God <alokender018@gmail.com>
2024-05-24 06:31:18 +0530

    hehe

--- src/injections/DisplayProfileUtils.ts
+++ src/injections/DisplayProfileUtils.ts
@@ -6,6 +6,7 @@ export default (): void => {
   const { DisplayProfileUtils } = Modules;
   PluginInjector.after(DisplayProfileUtils, "default", ([userId], res) => {
     if (
+      !res ||
       !USRDB.has(userId) ||
       (res?.banner && SettingValues.get("nitroBanner", defaultSettings.nitroBanner))
     )
