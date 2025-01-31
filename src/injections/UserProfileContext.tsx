7ee361efda631ad1d8bf7bafc7c82a02806e52aa
yofukashino <alokender018@gmail.com>
2025-01-31 07:09:53 +0530

    feat: self profile icon

--- src/injections/UserProfileContext.tsx
+++ src/injections/UserProfileContext.tsx
@@ -1,26 +1,35 @@
-import { util } from "replugged";
-import { users as UltimateUserStore } from "replugged/common";
+import { webpack } from "replugged";
 import { PluginInjector, SettingValues, USRDB } from "../index";
 import { defaultSettings } from "../lib/consts";
 import Modules from "../lib/requiredModules";
 import USRBGButton from "../Components/USRBGButton";
-import Types from "../types";
+
 export default (): void => {
   const { UserProfileContext } = Modules;
   PluginInjector.before(UserProfileContext, "render", (args) => {
     const [props] = args;
     if (!props?.children) return args;
     if (!Array.isArray(props?.children)) props.children = [props?.children];
-    const profileHeader = props?.children?.find?.((c) =>
+
+    const profileHeaderIndex = props?.children?.findIndex?.((c) =>
       /{profileType:\w+,children:\w+}=/.exec(c?.type?.toString()),
     );
+    if (profileHeaderIndex === -1) {
+      const ProfileHeader = webpack.getBySource<
+        React.ComponentType<{ profileType: string; children?: React.ReactElement[] }>
+      >(/{profileType:\w+,children:\w+}=/);
+      props?.children.unshift(<ProfileHeader profileType={props.profileType} />);
+    }
+
     if (
-      !profileHeader ||
       !props.user ||
       !USRDB.has(props.user.id) ||
       (props.user?.banner && SettingValues.get("nitroBanner", defaultSettings.nitroBanner))
     )
       return args;
+
+    const profileHeader = props?.children[profileHeaderIndex != -1 ? profileHeaderIndex : 0];
+
     if (!Array.isArray(profileHeader.props.children)) {
       profileHeader.props.children = [profileHeader.props.children];
     }
